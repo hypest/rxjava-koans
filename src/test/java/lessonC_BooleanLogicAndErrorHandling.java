8676b189da0a3810840d899471f7337dfe0c6760
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2015-09-30 00:53:59 +0300

    My answers to Lesson C

--- src/test/java/lessonC_BooleanLogicAndErrorHandling.java
+++ src/test/java/lessonC_BooleanLogicAndErrorHandling.java
@@ -23,13 +23,15 @@ import static util.LessonResources.ElevatorPassenger;
 public class lessonC_BooleanLogicAndErrorHandling {
 
     private static final Observable<?> ________ = null;
-    public int mSum;
+    public Integer mSum;
     public Boolean mBooleanValue;
 
     private int ____;
     private Object ______ = "";
     private Boolean _________;
     private TestSubscriber<Object> mSubscriber;
+
+    Object mThrowable;
 
     Func1<ElevatorPassenger, Boolean> _______ = elevatorPassenger -> false;
     Observable<Boolean> __________;
@@ -75,11 +77,13 @@ public class lessonC_BooleanLogicAndErrorHandling {
          * Hint: Check out the Public methods available on LessonResources.Elevator and passenger!
          */
 
-        Func1<ElevatorPassenger, Boolean> elevatorRule = passenger -> ____ + ____ < ____;
+        Func1<ElevatorPassenger, Boolean> elevatorRule = passenger -> elevator
+                .getTotalWeightInPounds() + passenger.getWeightInPounds() < Elevator
+                .MAX_CAPACITY_POUNDS;
         /**
          * Now all we need to do is to plug in the rule in takeWhile()
          */
-        elevatorQueueOne.takeWhile(_______).doOnNext(elevator::addPassenger).subscribe(mSubscriber);
+        elevatorQueueOne.takeWhile(elevatorRule).doOnNext(elevator::addPassenger).subscribe(mSubscriber);
         assertThat(elevator.getPassengerCount()).isGreaterThan(0);
         assertThat(elevator.getTotalWeightInPounds()).isLessThan(Elevator.MAX_CAPACITY_POUNDS);
         assertThat(elevator.getPassengerCount()).isEqualTo(2);
@@ -134,10 +138,10 @@ public class lessonC_BooleanLogicAndErrorHandling {
         /**
          * Do we have several servers that give the same data and we want the fastest of the two?
          */
-        Observable.amb(________, ________, ________).subscribe(mSubscriber);
+        Observable.amb(networkA, networkB, networkC).subscribe(mSubscriber);
         mSubscriber.awaitTerminalEvent();
         List<Object> onNextEvents = mSubscriber.getOnNextEvents();
-        assertThat(onNextEvents).contains("request took : " + ____ + " millis");
+        assertThat(onNextEvents).contains("request took : " + smallestNetworkLatency + " millis");
         assertThat(onNextEvents).hasSize(1);
 
         // bonus! we can call .cache() on an operation that takes a while. It will save the pipeline's events
@@ -156,7 +160,7 @@ public class lessonC_BooleanLogicAndErrorHandling {
         Observable.just(2, 4, 6, 8, 9)
                 .all(integer -> integer % 2 == 0)
                 .subscribe(aBoolean -> mBooleanValue = aBoolean);
-        assertThat(mBooleanValue).isEqualTo(____);
+        assertThat(mBooleanValue).isEqualTo(false);
     }
 
     /**
@@ -168,7 +172,7 @@ public class lessonC_BooleanLogicAndErrorHandling {
     public void _4_challenge_compositionMeansTheSumIsGreaterThanTheParts() {
         mSum = 0;
         Observable<Integer> range = Observable.range(1, 10);
-        //hmmmmmmmm.. how can we emit 1 value of 19 from the original range of numbers?
+        range.filter(integer -> integer > 8).forEach(integer1 -> mSum += integer1);
         assertThat(mSum).isEqualTo(19);
         //HINT: Could you use the MathObservable, with one of the operators you have learned about already to accomplish a result of 19?
     }
@@ -185,14 +189,13 @@ public class lessonC_BooleanLogicAndErrorHandling {
         List<String> arrayOne = new ArrayList<>();
         List<String> arrayTwo = new ArrayList<>();
         List<String> arrayThree = null;
-        Object mThrowable = null;
         Observable.just(arrayOne, arrayTwo, arrayThree).map(new Func1<List<String>, List<String>>() {
             @Override
             public List<String> call(List<String> strings) {
                 strings.add("GOOD JOB!");
                 return strings;
             }
-        }).doOnError(oops -> ______ = oops).subscribe(mSubscriber);
+        }).doOnError(oops -> mThrowable = oops).subscribe(mSubscriber);
         assertThat(mThrowable).isInstanceOf(Throwable.class);
     }
 
@@ -209,7 +212,7 @@ public class lessonC_BooleanLogicAndErrorHandling {
                 return networkAdapter.getData().get(0);
             }
         }).repeat(100);
-        networkRequestObservable.retry(____).subscribe(mSubscriber);
+        networkRequestObservable.retry(42).subscribe(mSubscriber);
         assertThat(mSubscriber.getOnNextEvents().get(0)).isEqualTo("extremely important data");
     }
 
@@ -225,11 +228,13 @@ public class lessonC_BooleanLogicAndErrorHandling {
         Observable<Boolean> tumbler3Observable = Observable.just(20).map(integer -> new Random().nextInt(20) > 15).delay(new Random().nextInt(20), TimeUnit.MILLISECONDS).repeat(1000);
 
         Func3<Boolean, Boolean, Boolean, Boolean> combineTumblerStatesFunction = (tumblerOneUp, tumblerTwoUp, tumblerThreeUp) -> {
-            Boolean allTumblersUnlocked = _________ && _________ && _________;
+            Boolean allTumblersUnlocked = tumblerOneUp && tumblerTwoUp && tumblerThreeUp;
             return allTumblersUnlocked;
         };
 
-        Observable<Boolean> lockIsPickedObservable = Observable.combineLatest(__________, __________, __________, combineTumblerStatesFunction).takeUntil(unlocked -> unlocked == true).last();
+        Observable<Boolean> lockIsPickedObservable = Observable.combineLatest(tumbler1Observable,
+                tumbler2Observable, tumbler1Observable, combineTumblerStatesFunction).takeUntil
+                (unlocked -> unlocked == true).last();
         lockIsPickedObservable.subscribe(mSubscriber);
         mSubscriber.awaitTerminalEvent();
         List<Object> onNextEvents = mSubscriber.getOnNextEvents();
